f950603bab0c3525dd768a3f4134c8362b535882
Philippe Brossier <brossierp@gmail.com>
2017-09-06 14:28:15 +0100

    CTPA-1557 to get survey details back

---
+++ src/main/java/uk/gov/ons/response/survey/representation/SurveyDTO.java
@@ -0,0 +1,18 @@
+package uk.gov.ons.response.survey.representation;
+
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Survey API representation
+ *
+ */
+@Data
+@NoArgsConstructor(access = AccessLevel.PUBLIC)
+public class SurveyDTO {
+  private String id;
+  private String shortName;
+  private String longName;
+  private String surveyRef;
+}
