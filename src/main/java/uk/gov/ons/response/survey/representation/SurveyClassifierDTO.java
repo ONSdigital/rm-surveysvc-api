6dc294f1226d508ba9b945888fb2d9e04083a6b2
Emilio Ward <emilioward@hotmail.co.uk>
2018-06-26 09:15:37 +0100

    added stylers

--- src/main/java/uk/gov/ons/response/survey/representation/SurveyClassifierDTO.java
+++ src/main/java/uk/gov/ons/response/survey/representation/SurveyClassifierDTO.java
@@ -4,15 +4,11 @@ import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-/**
- * SurveyClassifier API representation
- *
- */
+/** SurveyClassifier API representation */
 @Data
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
 public class SurveyClassifierDTO {
 
   private String id;
   private String name;
-
 }
